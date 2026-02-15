3aea362e14066cbb5fd7f6878c89979e1c0baf89
ChinhTC <assin2910@gmail.com>
2026-02-15 20:01:31 +0700

    feat: add background

---
+++ src/components/BackgroundGlow.tsx
@@ -0,0 +1,7 @@
+export default function BackgroundGlow() {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      {/* <div className="w-[500px] h-[500px] rounded-full bg-yellow-400 opacity-20 blur-3xl animate-pulse" /> */}
+    </div>
+  );
+}
